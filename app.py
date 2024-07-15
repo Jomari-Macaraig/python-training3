c759ee7da140580218998abf27aebe6c50ccfa1c
Jomari Macaraig <jomari@auxahelth.com>
2024-07-15 22:45:44 +0800

    topic2: Methods

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
 # class Employee:
-#     pass
-#
-# employee1 = Employee()
-# employee2 = Employee()
-#
-# print(employee1)
-# print(employee2)
-#
-# employee1.first_name = "David"
-# employee1.last_name = "Smith"
-# employee1.email = "david.smith@company.com"
-# employee1.pay = 1000
-#
-# employee2.first_name = "Test"
-# employee2.last_name = "User"
-# employee2.email = "test.user@company.com"
-# employee2.pay = 2000
-#
-# print(employee1.email)
-# print(employee2.email)
-#
-#
-# class Employee:
 #
 #     def __init__(self, first_name, last_name, pay):
 #         self.first_name = first_name
 #         self.last_name = last_name
 #         self.pay = pay
 #         self.email = f"{first_name}.{last_name}@company.com"
+#
+#     def get_fullname(self):
+#         return f"{self.first_name} {self.last_name}"
 #
 # employee1 = Employee(
 #     first_name="David",
@@ -43,3 +23,12 @@
 #
 # print(employee1.email)
 # print(employee2.email)
+
+# print(f"{employee1.first_name} {employee1.last_name}")
+# print(f"{employee2.first_name} {employee2.last_name}")
+
+# print(employee1.get_fullname())
+# print(employee2.get_fullname())
+
+# print(Employee.get_fullname(employee1))
+# print(Employee.get_fullname(employee2))
