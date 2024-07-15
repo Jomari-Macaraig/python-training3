aea445a8a6c285db66a57bb15a336a65f77a4079
Jomari Macaraig <jomari@auxahelth.com>
2024-07-15 22:36:11 +0800

    topic1: Basic class

---
+++ app.py
@@ -0,0 +1,45 @@
+# class Employee:
+#     pass
+#
+# employee1 = Employee()
+# employee2 = Employee()
+#
+# print(employee1)
+# print(employee2)
+#
+# employee1.first_name = "David"
+# employee1.last_name = "Smith"
+# employee1.email = "david.smith@company.com"
+# employee1.pay = 1000
+#
+# employee2.first_name = "Test"
+# employee2.last_name = "User"
+# employee2.email = "test.user@company.com"
+# employee2.pay = 2000
+#
+# print(employee1.email)
+# print(employee2.email)
+#
+#
+# class Employee:
+#
+#     def __init__(self, first_name, last_name, pay):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.pay = pay
+#         self.email = f"{first_name}.{last_name}@company.com"
+#
+# employee1 = Employee(
+#     first_name="David",
+#     last_name="Smith",
+#     pay=1000
+# )
+#
+# employee2 = Employee(
+#     first_name="Test",
+#     last_name="User",
+#     pay=1000
+# )
+#
+# print(employee1.email)
+# print(employee2.email)
